bdaa6b611a492e0cd462903a5e6ee3ae2c1080c0
Jeremy new computer <jeremy8@vt.edu>
2023-06-09 17:15:47 -0400

    added a section where you can add comments

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
-import { Avatar, Box, Divider, Typography, useTheme, IconButton, useMediaQuery  } from "@mui/material";
-import { FavoriteBorderOutlined, FavoriteOutlined, ChatBubbleOutlineOutlined, ShareOutlined } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Typography,
+  useTheme,
+  IconButton,
+  useMediaQuery,
+  TextField,
+} from "@mui/material";
+import {
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ChatBubbleOutlineOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
 
 import { useDispatch, useSelector } from "react-redux";
 import { setPost } from "state";
@@ -20,14 +33,13 @@ const PostWidget = ({
   comments,
 }) => {
   const [isComments, setIsComments] = React.useState(false);
+  const [newComment, setNewComment] = React.useState("");
   const dispatch = useDispatch();
   const token = useSelector((state) => state.token);
   const loggedInUserId = useSelector((state) => state.user._id);
   const isLiked = Boolean(likes[loggedInUserId]);
   const likeCount = Object.keys(likes).length;
-  
-  
-  
+
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const { palette } = useTheme();
   const main = palette.neutral.main;
@@ -46,9 +58,33 @@ const PostWidget = ({
     dispatch(setPost({ post: updatedPost }));
   };
 
+  const handleCommentChange = (event) => {
+    setNewComment(event.target.value);
+  };
+
+  const handleCommentSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(`http://localhost:3001/posts/${postId}/comment`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: loggedInUserId, comment: newComment }),
+    });
+
+    const updatedPost = await response.json();
+    dispatch(setPost({ post: updatedPost }));
+    setNewComment("");
+  };
+
   return (
-    <div className="flex flex-col p-3 mb-5 rounded-lg border bg-zinc-200 drop-shadow-lg border-b shadow-2xl" style={{ marginBottom: '40px', justifyContent: 'space-between' }}>
-      <div className="flex flex-col justify-between mr-2" >
+    <div
+      className="flex flex-col p-3 mb-5 rounded-lg border bg-zinc-200 drop-shadow-lg border-b shadow-2xl"
+      style={{ marginBottom: "40px", justifyContent: "space-between" }}
+    >
+      <div className="flex flex-col justify-between mr-2">
         <div className="flex items-center">
           <Friend
             friendId={postUserId}
@@ -58,11 +94,9 @@ const PostWidget = ({
           />
         </div>
         <Divider sx={{ mb: 2 }} />
-        <p style={{ wordBreak: 'break-word' }}>
-          {description}
-        </p>
+        <p style={{ wordBreak: "break-word" }}>{description}</p>
       </div>
-  
+
       <div>
         {picturePath && (
           <img
@@ -73,7 +107,7 @@ const PostWidget = ({
         )}
       </div>
       <Divider sx={{ mt: 2 }} />
-  
+
       <FlexBetween mt="0.25rem">
         <FlexBetween gap="1rem">
           <FlexBetween gap="0.3rem">
@@ -97,7 +131,7 @@ const PostWidget = ({
           <ShareOutlined />
         </IconButton>
       </FlexBetween>
-  
+
       {isComments && (
         <Box mt="0.5rem">
           {comments.map((comment, i) => (
@@ -109,11 +143,19 @@ const PostWidget = ({
             </Box>
           ))}
           <Divider />
+          <form onSubmit={handleCommentSubmit}>
+            <TextField
+              value={newComment}
+              onChange={handleCommentChange}
+              label="Add a comment"
+              variant="outlined"
+              fullWidth
+            />
+          </form>
         </Box>
       )}
     </div>
   );
-  
 };
 
 export default PostWidget;
