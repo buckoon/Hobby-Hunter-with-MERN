b2ee3bd4ab3bb673f184633d7a21b7ab373defa0
Jeremy new computer <jeremy8@vt.edu>
2023-06-10 12:53:33 -0400

    Added and commented out code needed later for the user profile page.

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
 import PostWidget from "./PostWidget";
 
-const PostsWidget = ({ userId, isProfile = false }) => {
-  const dispatch = useDispatch();  //so we can use Redux
+const PostsWidget = (/*{ userId, isProfile = false }*/) => {
+  const dispatch = useDispatch(); 
   const posts = useSelector((state) => state.posts); //so we can grab the store list of posts
   const token = useSelector((state) => state.token); //so we can grab the token as well from Redux
 
@@ -17,7 +17,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     dispatch(setPosts({ posts: data }));//updates store
   };
 
-  const getUserPosts = async () => {
+ /* const getUserPosts = async () => {
     const response = await fetch(
       `http://localhost:3001/posts/${userId}/posts`, 
       {
@@ -27,15 +27,19 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     );
     const data = await response.json();
     dispatch(setPosts({ posts: data }));
-  };
+  };*/
 
-  useEffect(() => {
+  /*useEffect(() => {
     if (isProfile) {
       getUserPosts();
     } else {
       getPosts();
     }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);*/
+
+  useEffect(() => {
+   getPosts();
+    }, []);
 
   return (
     <>
