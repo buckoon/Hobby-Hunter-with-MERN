708486c6456a68f347c2be4f42e27529c238ee0f
Jeremy new computer <jeremy8@vt.edu>
2023-07-22 12:51:56 -0400

    made a profile page.

--- src/scenes/profilePage/index.jsx
+++ src/scenes/profilePage/index.jsx
@@ -1,5 +1,73 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navbar";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import UserWidget from "scenes/widgets/UserWidget";
+
 const ProfilePage = () => {
-    return <div>ProfilePage</div>;
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+  const getUser = async () => {
+    const response = await fetch(`https://hobby-hunter-api.onrender.com/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); 
+
+  if (!user) return null;
+
+  return (
+    <div className ="relative">
+        <div className="absolute top-0 left-0 w-full h-full z-0">
+            <img
+                className="fixed top-0 left-0 w-full h-full object-cover overflow-hidden z-[-1]"
+                src="https://cdn.osxdaily.com/wp-content/uploads/2017/06/macos-high-sierra-default-wallpaper-fall-mountain-scene-1.jpg"
+                alt="/"
+            />
+
+        <div className="bg-black/40 absolute top-0 left-0 w-full h-full"></div>
+      </div>
+     
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="2rem"
+        justifyContent="center"
+        position="relative"
+        zIndex={10}
+
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={userId} picturePath={user.picturePath} />
+          <Box m="2rem 0" />
+          <FriendListWidget userId={userId} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={user.picturePath} />
+          <Box m="2rem 0" />
+          <PostsWidget userId={userId} isProfile />
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default ProfilePage;
